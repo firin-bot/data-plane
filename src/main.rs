a5565c4c1ed2b37370008340051d3e640a22a652
Ori Sky Farrell <git@ori.mx>
2025-08-29 10:49:35 +0100

    Send session ID to control plane!

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,18 @@
-use anyhow::Result;
+use anyhow::{Context, Result};
 use futures_util::StreamExt as _;
 use rand::Rng as _;
 use tokio::time::{Duration, Instant};
 use tokio_tungstenite::tungstenite::client::IntoClientRequest as _;
+use twitch_api::eventsub::{
+    Event,
+    EventsubWebsocketData
+};
+
+struct DataState {
+    control_host: String,
+    control_port: u16,
+    control_token: String
+}
 
 fn jitter(d: Duration, percent: f64) -> Duration {
     let factor = rand::rng().random_range(0.0..percent);
@@ -14,12 +24,22 @@ async fn main() -> Result<()> {
     env_logger::init();
     dotenvy::dotenv().ok();
 
+    let control_host  = std::env::var("CONTROL_HOST" ).context("missing CONTROL_HOST")?;
+    let control_port  = std::env::var("CONTROL_PORT" ).context("missing CONTROL_PORT")?.parse::<u16>()?;
+    let control_token = std::env::var("CONTROL_TOKEN").context("missing CONTROL_TOKEN")?;
+
     const MIN_BACKOFF: Duration = Duration::from_secs(1);
     const MAX_BACKOFF: Duration = Duration::from_secs(30);
     const GOOD_SESSION: Duration = Duration::from_secs(30);
 
     let request = "wss://eventsub.wss.twitch.tv/ws".into_client_request()?;
     let mut backoff = MIN_BACKOFF;
+
+    let data_state = DataState {
+        control_host,
+        control_port,
+        control_token
+    };
 
     loop {
         let started = Instant::now();
@@ -35,8 +55,11 @@ async fn main() -> Result<()> {
             }
         };
 
-        while let Some(item) = stream.next().await {
-            log::info!("{item:#?}");
+        while let Some(Ok(msg)) = stream.next().await {
+            if let Err(e) = handle_message(&data_state, msg).await {
+                log::error!("{e}");
+                break
+            }
         }
 
         let uptime = started.elapsed();
@@ -49,5 +72,44 @@ async fn main() -> Result<()> {
         let b = jitter(backoff, 0.5);
         log::error!("disconnected; reconnecting in {} seconds", b.as_secs());
         tokio::time::sleep(b).await;
+    }
+}
+
+async fn handle_message(data_state: &DataState, msg: tungstenite::Message) -> Result<()> {
+    match msg {
+        tungstenite::Message::Text(text) => {
+            match Event::parse_websocket(&text)? {
+                EventsubWebsocketData::Welcome {
+                    payload,
+                    ..
+                } => {
+                    log::info!("websocket session ID = {:#?}", payload.session.id);
+                    let client = reqwest::Client::new();
+                    let url = reqwest::Url::parse(&format!(
+                        "http://{}:{}/session/assign",
+                        data_state.control_host,
+                        data_state.control_port
+                    ))?;
+                    let res = client
+                        .post(url)
+                        .bearer_auth(data_state.control_token.clone())
+                        .body(payload.session.id.as_ref().to_owned())
+                        .send().await?;
+                    log::info!("{res:?}");
+                },
+                EventsubWebsocketData::Notification {
+                    payload,
+                    ..
+                } => {
+                    log::info!("{payload:#?}");
+                }
+                _ => {}
+            }
+            Ok(())
+        },
+        tungstenite::Message::Close(_) => {
+            anyhow::bail!("connection closed by server")
+        },
+        _ => Ok(())
     }
 }
